9c4037ed7ddb91e4da3975dd447ff58ef1236d2b
Billy Montgomery <billy.montgomery@gmail.com>
2020-02-16 14:56:15 -0800

    add readme and prettier fixes

--- src/useWaiter.js
+++ src/useWaiter.js
@@ -1,59 +1,59 @@
-import { useState, useEffect } from 'react'
-import getTime from './helpers/getTime'
+import { useState, useEffect } from 'react';
+import getTime from './helpers/getTime';
 
 export default function useWaiter(requestCreator) {
   // waiter lifecyle
-  const [isPending, setPending] = useState(false)
-  const [isResolved, setResolved] = useState(false)
-  const [isRejected, setRejected] = useState(false)
-  const [isCompleted, setCompleted] = useState(false)
+  const [isPending, setPending] = useState(false);
+  const [isResolved, setResolved] = useState(false);
+  const [isRejected, setRejected] = useState(false);
+  const [isCompleted, setCompleted] = useState(false);
 
   // waiter timestamps
-  const [startTime, setStartTime] = useState(null)
-  const [endTime, setEndTime] = useState(null)
-  const [elapsedTime, setElapsedTime] = useState(null)
-  const [lastModified, setLastModified] = useState(null)
+  const [startTime, setStartTime] = useState(null);
+  const [endTime, setEndTime] = useState(null);
+  const [elapsedTime, setElapsedTime] = useState(null);
+  const [lastModified, setLastModified] = useState(null);
 
   // waiter
-  const [request, setRequest] = useState(null)
-  const [response, setResponse] = useState(null)
-  const [error, setError] = useState(null)
+  const [request, setRequest] = useState(null);
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function requestRunner() {
       try {
-        const request = requestCreator()
-        setRequest(request)
-        setStartTime(getTime())
-        setLastModified(getTime())
+        const request = requestCreator();
+        setRequest(request);
+        setStartTime(getTime());
+        setLastModified(getTime());
 
-        setPending(true)
+        setPending(true);
 
-        const data = await request
-        setResponse(data)
-        setError(null)
+        const data = await request;
+        setResponse(data);
+        setError(null);
 
-        setPending(false)
-        setResolved(true)
-        setRejected(false)
-        setCompleted(true)
-      } catch(e) {
-        setResponse(null)
-        setError(e)
+        setPending(false);
+        setResolved(true);
+        setRejected(false);
+        setCompleted(true);
+      } catch (e) {
+        setResponse(null);
+        setError(e);
 
-        setPending(false)
-        setResolved(false)
-        setRejected(true)
-        setCompleted(true)
+        setPending(false);
+        setResolved(false);
+        setRejected(true);
+        setCompleted(true);
       }
 
-      setEndTime(getTime())
-      setLastModified(getTime())
-      setElapsedTime(endTime - startTime)
+      setEndTime(getTime());
+      setLastModified(getTime());
+      setElapsedTime(endTime - startTime);
     }
 
-    requestRunner()
-  }, [])
+    requestRunner();
+  }, []);
 
   return {
     request,
@@ -68,5 +68,5 @@ export default function useWaiter(requestCreator) {
     lastModified,
     startTime,
     endTime,
-  }
+  };
 }
