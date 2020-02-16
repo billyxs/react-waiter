924f4e64bc23853cded4e16895fe87829a806464
Billy Montgomery <billy.montgomery@gmail.com>
2020-02-15 22:13:35 -0800

    feat/use-waiter-lifecycle: implement waiter response handling
    feat/use-waiter-lifecycle: add request handling with resolve/reject

--- src/useWaiter.js
+++ src/useWaiter.js
@@ -1,7 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-export default function useWaiter(request) {
-  const {response, setResponse} = useState(null)
+export default function useWaiter(requestCreator) {
+  // waiter result
+  const [response, setResponse] = useState(null)
+  const [error, setError] = useState(null)
 
-  return { response }
+  useEffect(() => {
+    async function requestRunner() {
+      try {
+        const data = await requestCreator()
+        setResponse({ success: true })
+      } catch(e) {
+        setError(e)
+      }
+    }
+
+    requestRunner()
+  }, [])
+
+  return {
+    response,
+    error,
+  }
 }
