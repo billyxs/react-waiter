aec27b2db061bb2a458b782322ef8c32220659b1
Billy Montgomery <billy.montgomery@gmail.com>
2020-02-15 23:52:42 -0800

    feat/use-waiter-lifecycle: update hook test with testing-library

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,34 +1,14 @@
 import React from 'react';
-import Enzyme, { shallow, mount } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import {render, fireEvent} from '@testing-library/react'
+import {renderHook, act} from '@testing-library/react-hooks'
 import { useWaiter } from '../src';
 
-Enzyme.configure({ adapter: new Adapter() });
-
-export default function testHook(
-  runHook,
-  flushEffects = true
-) {
-  function HookWrapper() {
-    const { response, error } = runHook();
-
-    return (
-      <span
-        response={response}
-        error={error}
-        />
-    );
-  }
-
-  const wrapperFunc = flushEffects ? mount : shallow;
-  const wrapper = wrapperFunc(<HookWrapper />);
-
-  return {
-    getWrapper: () => wrapper,
-    getProps: () => {
-      return wrapper.find('span').props()
-    }
-  }
+function createHookTest(promiseFunc) {
+  return renderHook(
+    () => useWaiter(
+      () => new Promise(promiseFunc)
+    )
+  )
 }
 
 describe('useWaiter', () => {
@@ -36,46 +16,31 @@ describe('useWaiter', () => {
     expect(typeof useWaiter).toBe('function');
   });
 
-  describe('response', () => {
-    it('should initialize as null', () => {
-      const { getProps } = testHook(
-          () => useWaiter(() => Promise.resolve()),
-          false
+  it('should handle response state', async () => {
+    const MOCK_RESPONSE = { success: true }
+    const { result, waitForNextUpdate, } = createHookTest(
+      resolve => setTimeout(
+        () => resolve((MOCK_RESPONSE)), 500
       )
-      expect(getProps().response).toBe(null);
-      expect(getProps().error).toBe(null);
-    })
-
-    it('should set response', async () => {
-      const MOCK_RESPONSE = { success: true }
-      const { getWrapper, getProps } = testHook(
-          () => useWaiter(() => Promise.resolve(MOCK_RESPONSE)),
-          true
-      )
-      await new Promise((resolve) => setTimeout(resolve, 100))
-      getWrapper().update()
-      expect(getProps().response).toBe(MOCK_RESPONSE);
-    })
+    )
+    expect(result.current.response).toBe(null);
+    expect(result.current.error).toBe(null);
+    await waitForNextUpdate()
+    expect(result.current.response).toBe(MOCK_RESPONSE);
+    expect(result.current.error).toBe(null);
   })
 
-  describe('error', () => {
-    it('should initialize as null', () => {
-      const { getProps } = testHook(
-          () => useWaiter(() => Promise.resolve()),
-          false
+  it('should handle error state', async () => {
+    const MOCK_ERROR = { message: 'error' }
+    const { result, waitForNextUpdate, } = createHookTest(
+      (resolve, reject) => setTimeout(
+        () => reject((MOCK_ERROR)), 500
       )
-      expect(getProps().response).toBe(null);
-    })
-
-    it('should set error', async () => {
-      const MOCK_ERROR = { message: 'error' }
-      const { getWrapper, getProps } = testHook(
-          () => useWaiter(() => Promise.reject(MOCK_ERROR)),
-          true
-      )
-      await new Promise((resolve) => setTimeout(resolve, 100))
-      getWrapper().update()
-      expect(getProps().error).toBe(MOCK_ERROR);
-    })
+    )
+    expect(result.current.response).toBe(null);
+    expect(result.current.error).toBe(null);
+    await waitForNextUpdate()
+    expect(result.current.response).toBe(null);
+    expect(result.current.error).toBe(MOCK_ERROR);
   })
 });
