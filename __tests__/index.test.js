108bebad1ccb9366858ca6413e53aebbf0dc7527
Billy Montgomery <billym@yelp.com>
2020-02-15 22:01:44 -0800

    feat/use-waiter-lifecycle: add hook usage to waiter with test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,33 @@
+import React from 'react';
+import Enzyme, { shallow, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import { useWaiter } from '../src';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+export default function testHook(runHook, flushEffects = true) {
+  function HookWrapper() {
+    const output = runHook();
+
+    return (
+      <span output={output} />
+    );
+  }
+
+  const wrapperFunc = flushEffects ? mount : shallow;
+  const wrapper = wrapperFunc(<HookWrapper />);
+
+  return wrapper.find('span').props().output;
+}
 
 describe('useWaiter', () => {
   it('should be a function', () => {
     expect(typeof useWaiter).toBe('function');
-    expect(useWaiter()).toBeUndefined();
   });
+
+  it('should', () => {
+    const value = testHook(() => useWaiter(), false)
+    expect(value.response).toBe(undefined);
+
+  })
 });
--- src/useWaiter.js
+++ src/useWaiter.js
@@ -1,1 +1,7 @@
-export default function useWaiter() {}
+import { useState } from 'react'
+
+export default function useWaiter(request) {
+  const {response, setResponse} = useState(null)
+
+  return { response }
+}
