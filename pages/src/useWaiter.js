48a1f9c647b3eaa2780d3197d8d49d46645398f8
Billy Montgomery <billy.montgomery@gmail.com>
2020-02-20 15:00:44 -0800

    feat/is-refreshing: add tests and live example of refreshing

--- pages/src/useWaiter.js
+++ pages/src/useWaiter.js
@@ -7,12 +7,14 @@ export function useWaiter(requestCreator, requestParams) {
   // mutables
   const id = useRef(null);
   const params = useRef(null);
+  const response = useRef(null);
 
   // waiter lifecyle
   const [isPending, setPending] = useState(false);
   const [isResolved, setResolved] = useState(false);
   const [isRejected, setRejected] = useState(false);
   const [isCompleted, setCompleted] = useState(false);
+  const [isRefreshing, setRefreshing] = useState(false);
 
   // waiter timestamps
   const [startTime, setStartTime] = useState(null);
@@ -21,19 +23,22 @@ export function useWaiter(requestCreator, requestParams) {
 
   // waiter request data
   const [request, setRequest] = useState(null);
-  const [response, setResponse] = useState(null);
   const [error, setError] = useState(null);
 
   async function requestRunner(runnerParams) {
-    // update the request params used in request
-    params.current = runnerParams
-
-    // increment id on each call
+    params.current = runnerParams;
     const waiterId = id.current + 1;
     id.current = waiterId;
 
     try {
-      const request = requestCreator(runnerParams);
+      const request = requestCreator();
+      // if we have a response already, we're refreshing
+      console.log('response = ', response.current)
+      console.log('isResolved = ', isResolved)
+      const refresh = !!response.current
+      console.log('refresh = ', refresh)
+      setRefreshing(refresh)
+
       // waiter request init
       setRequest(request);
       setError(null);
@@ -54,14 +59,14 @@ export function useWaiter(requestCreator, requestParams) {
         return;
       }
       // waiter success changes
-      setResponse(data);
+      response.current = data;
       setResolved(true);
     } catch (e) {
       if (waiterId !== id.current) {
         return;
       }
       // request error changes
-      setResponse(null);
+      response.current = null
       setError(e);
       setRejected(true);
     }
@@ -69,6 +74,7 @@ export function useWaiter(requestCreator, requestParams) {
     // waiter completed changes
     setPending(false);
     setCompleted(true);
+    setRefreshing(false);
     setEndTime(getTime());
     setLastModified(getTime());
   }
@@ -87,13 +93,14 @@ export function useWaiter(requestCreator, requestParams) {
 
     id: id.current,
     request,
-    response,
+    response: response.current,
     error,
 
     isPending,
     isResolved,
     isRejected,
     isCompleted,
+    isRefreshing,
 
     lastModified,
     startTime,
